e3d20c47a6edc9ebf3bc280bd7fb4d7247633b54
Kenta Goto <kenta9736@gmail.com>
2025-09-04 01:17:26 +0900

    Auth0との認証に必要なミドルウェア等の実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
+import { Auth0Provider } from "@auth0/nextjs-auth0";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ja">
+      <Auth0Provider>
+        <body>{children}</body>
+      </Auth0Provider>
     </html>
   );
 }
