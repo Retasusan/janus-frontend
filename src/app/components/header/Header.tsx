3fcfdb32d01bf834c594ff5206bf6aec6e678605
Kenta Goto <kenta9736@gmail.com>
2025-09-05 22:49:39 +0900

    ドロワーを追加

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -2,36 +2,36 @@
 
 import { IoIosArrowDown } from "react-icons/io";
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header() {
   return (
     <header className="bg-white h-10 pl-2 pr-5 py-2 flex justify-between">
       <div>logo</div>
-      <button type="button" className="text-xs flex justify-between gap-2 mt-1">
+      {/* <button type="button" className="text-xs flex justify-between gap-2 mt-1">
         山田太郎
         <IoIosArrowDown className="mt-[1.5px]" />
-      </button>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+      </button> */}
+      <DropdownMenu>
+        <DropdownMenuTrigger className="text-xs flex justify-between gap-2 m-1">
+          山田太郎
+          <IoIosArrowDown className="mt-[2.2px]" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
