9eb39691c0506ccb1cbd3009561e9fcb98d2a4cd
Kenta Goto <kenta9736@gmail.com>
2025-09-05 23:56:18 +0900

    不要な文を削除

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,10 +14,6 @@ export default function Header() {
   return (
     <header className="bg-white h-10 pl-2 pr-5 py-2 flex justify-between">
       <div>logo</div>
-      {/* <button type="button" className="text-xs flex justify-between gap-2 mt-1">
-        山田太郎
-        <IoIosArrowDown className="mt-[1.5px]" />
-      </button> */}
       <DropdownMenu>
         <DropdownMenuTrigger className="text-xs flex justify-between gap-2 m-1">
           山田太郎
